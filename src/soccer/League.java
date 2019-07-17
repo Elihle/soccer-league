b269c03cc6ed61eb0793d488b012ee5aaf19d512
Coder <lihlemenz@gmail.com>
2019-07-17 11:43:32 +0200

    added team2

--- src/soccer/League.java
+++ src/soccer/League.java
@@ -20,13 +20,22 @@ public class League {
         team1.playerArray = thePlayers;
 
 
-//        Team team2 = new Team();
-//        team2.teamName = "Man United";
-//        team2.playerArray = new Player[3];
-//        team2.playerArray[0] = new Player();
-//        team2.playerArray.PlayerName = "";
+        Team team2 = new Team();
+        team2.teamName = "Man United";
+        team2.playerArray = new Player[3];
+        team2.playerArray[0] = new Player();
+        team2.playerArray[0].PlayerName = "Sine Molo";
+
+        team2.playerArray[1] = new Player();
+        team2.playerArray[1].PlayerName = "Marshall Mathers";
+        team2.playerArray[2] = new Player();
+        team2.playerArray[2].PlayerName = "Lihle Carter";
 
         for (Player thePlayer : team1.playerArray) {
+            System.out.println(thePlayer.PlayerName);
+        }
+
+        for (Player thePlayer : team2.playerArray) {
             System.out.println(thePlayer.PlayerName);
         }
 
